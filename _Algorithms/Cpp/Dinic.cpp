8a069b6caa638a04b2bf502316f61275c024c618
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2021-02-23 20:05:32 +0900

    Add Dinic.cpp

---
+++ _Algorithms/Cpp/Dinic.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Dinic {
+    struct Edge {
+        int v, cap, rev;
+        Edge(int v, int cap, int rev) : v(v), cap(cap), rev(rev) {}
+    };
+
+    const int INF = 2e9;
+    int MAX_V;
+    int S, E;  // source, sink
+    vector<vector<Edge>> adj;
+    vector<int> level, work;
+
+    bool bfs() {
+        fill(level.begin(), level.end(), -1);
+        queue<int> qu;
+        level[S] = 0;
+        qu.push(S);
+        while (qu.size()){
+            int here = qu.front();
+            qu.pop();
+            for (auto i : adj[here]) {
+                int there = i.v;
+                int cap = i.cap;
+                if (level[there] == -1 && cap > 0) {
+                    level[there] = level[here] + 1;
+                    qu.push(there);
+                }
+            }
+        }
+        return level[E] != -1;
+    }
+
+    int dfs(int here, int crtcap) {
+        if (here == E) return crtcap;
+        for (int &i = work[here]; i < int(adj[here].size()); i++) {
+            int there = adj[here][i].v;
+            int cap = adj[here][i].cap;
+            if (level[here] + 1 == level[there] && cap > 0) {
+                int c = dfs(there, min(crtcap, cap));
+                if (c > 0) {
+                    adj[here][i].cap -= c;
+                    adj[there][adj[here][i].rev].cap += c;
+                    return c;
+                }
+            }
+        }
+        return 0;
+    }
+
+public:
+    Dinic(int MAX_V) : MAX_V(MAX_V) {
+        adj.resize(MAX_V);
+        level.resize(MAX_V);
+        work.resize(MAX_V);
+    }
+    void add_edge(int s, int e, int c) {
+        adj[s].emplace_back(e, c, (int)adj[e].size());
+        adj[e].emplace_back(s, 0, (int)adj[s].size() - 1);
+    }
+
+    int get_max_flow(int s, int e) {
+        S = s, E = e;
+        int res = 0;
+        while (bfs()) {
+            fill(work.begin(), work.end(), 0);
+            while (1) {
+                int flow = dfs(S, INF);
+                if (!flow) break;
+                res += flow;
+            }
+        }
+        return res;
+    }
+};
